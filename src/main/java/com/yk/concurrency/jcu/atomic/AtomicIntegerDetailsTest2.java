67dacfbd0c0f48130ebe3b6747c173bd738d1c31
YuKaiFan <michaelkai@aliyun.com>
2020-10-15 22:51:52 +0800

    新增AtomicInteger相关知识

--- src/main/java/com/yk/concurrency/jcu/atomic/AtomicIntegerDetailsTest2.java
+++ src/main/java/com/yk/concurrency/jcu/atomic/AtomicIntegerDetailsTest2.java
@@ -10,7 +10,7 @@ public class AtomicIntegerDetailsTest2 {
     private final static CompareAndSetLock LOCK = new CompareAndSetLock();
 
     public static void main(String[] args) {
-        for (int  i = 0; i < 2; i++) {
+        for (int  i = 0; i < 5; i++) {
             new Thread(() -> {
                 try {
                     doSomething2();
@@ -21,6 +21,10 @@ public class AtomicIntegerDetailsTest2 {
         }
     }
 
+    /**
+     * 这种方式, 一个线程抢到锁, 其他线程就处于blocked状态
+     * @throws InterruptedException
+     */
     private static void doSomething() throws InterruptedException {
         synchronized (AtomicIntegerDetailsTest2.class) {
             System.out.println(Thread.currentThread().getName() + "get the lock");
@@ -30,6 +34,8 @@ public class AtomicIntegerDetailsTest2 {
 
     /**
      * 自定义tryLock显示锁, 利用AtomicInteger的compareAndSet()方法
+     *
+     * 如果一个线程抢到了锁, 那么其他线程就会直接抛出异常, 而不是blocked
      * @throws InterruptedException
      * @throws GetLockException
      */
--- src/main/java/com/yk/concurrency/jcu/atomic/CompareAndSetLock.java
+++ src/main/java/com/yk/concurrency/jcu/atomic/CompareAndSetLock.java
@@ -11,10 +11,15 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class CompareAndSetLock {
     private final AtomicInteger value = new AtomicInteger(0);
 
+    private Thread lockedThread;
+
     public void tryLock() throws GetLockException{
         boolean b = value.compareAndSet(0, 1);
         if (!b) {
             throw new GetLockException("Get tge Lok failed");
+        } else {
+            //设置排它锁, 在释放锁时, 判断线程是否是当前线程, 如果不加这个, 那么其他线程在unlock()时, 又会把这个锁释放, 导致其他线程又可以获取锁。
+            lockedThread = Thread.currentThread();
         }
     }
 
@@ -22,6 +27,8 @@ public class CompareAndSetLock {
         if (0 == value.get()) {
             return;
         }
-        value.compareAndSet(1, 0);
+        if (lockedThread == Thread.currentThread()) {
+            value.compareAndSet(1, 0);
+        }
     }
 }
